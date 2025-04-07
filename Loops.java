b7dfd69cbf5a70a3467b953560c9a07cfe062c37
Angela Aya Armah-Sekum <157904102+aaarmahsekum@users.noreply.github.com>
2025-04-07 08:19:16 +0000

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -1,7 +1,7 @@
 public class Loops {
     public static void main(String[] args) {
         int i;
-        for (i = 2; i < 25; i--) {
+        for (i = 2; i < 25; i++) {
             if (i == 15)break;
                 
             System.out.println(i * 2);
